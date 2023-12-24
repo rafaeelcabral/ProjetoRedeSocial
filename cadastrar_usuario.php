e8b8f7907706fa1c061d800c607f8afe2c6ccfb8
Rafael Cabral <melloraf.cabral@gmail.com>
2023-12-24 03:37:47 -0300

    subindo projeto

---
+++ cadastrar_usuario.php
@@ -0,0 +1,104 @@
+<?php
+  // Dados de conexão com o banco de dados
+  $servername = "localhost"; // endereço do servidor
+  $username = "root"; // nome de usuário do banco de dados
+  $password = ""; // senha do banco de dados
+  $dbname = "ProjetoRedeSocial"; // nome do banco de dados
+
+  // Mensagens de feedback
+  $mensagem = '';
+  $mensagem_link = '';
+
+  // Verifica se o formulário foi enviado
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recebe os dados do formulário
+    $email = $_POST['email'];
+    $usuario = $_POST['username'];
+    $senha = $_POST['password'];
+
+    // Cria a conexão com o banco de dados
+    $conexao = new mysqli($servername, $username, $password, $dbname);
+
+    // Verifica se houve erro na conexão
+    if ($conexao === false) {
+      die("Erro de conexão com o banco de dados: " . mysqli_connect_error());
+    }
+
+    // Insere os dados na tabela "Usuario"
+    $sql = "INSERT INTO Usuario (email, username, senha) VALUES ('$email', '$usuario', '$senha')";
+
+    if ($conexao->query($sql) === TRUE) {
+      $mensagem = "Usuário cadastrado com sucesso!";
+      $mensagem_link = "Clique <a href='login.html'>aqui</a> para retornar à página de login.";
+    } else {
+      $mensagem = "Erro ao cadastrar usuário: " . $conn->error;
+    }
+
+    // Fecha a conexão com o banco de dados
+    $conexao->close();
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cadastro de Usuário</title>
+
+    <style>
+
+      body{
+        background: url(img/fundo.jpg);
+      } 
+
+      h2{
+        padding-left: 20px;
+        font-weight: bold;
+        color: orange;
+      }
+
+      p{
+        padding-left: 20px;
+        font-weight: bold;
+        color: navy;
+      }
+
+      p a{
+        color: orange;
+      }
+
+      .mensagem{
+        width: 30%;
+	      margin: 80px auto 10px auto;
+        padding:30px;
+        background: rgba(162, 170, 172, .4);
+        border-radius: 20px;
+      }
+
+      /* @media para telas "medium" */
+      @media (max-width: 992px) {
+        .mensagem {
+          width: 50%;
+        }
+      }
+
+      /* @media para telas "small" */
+      @media (max-width: 768px) {
+        .mensagem {
+          width: 70%;
+        }
+      }     
+
+    </style>
+</head>
+<body>
+
+    <div class="mensagem">
+        <h2>Cadastro de Usuário</h2>
+        <p><?php echo $mensagem; ?></p><br>
+        <hr>
+        <p><?php echo $mensagem_link; ?></p>
+        <hr>
+    </div>  
+
+</body>
+</html>
---
+++ verificar_login.php
@@ -0,0 +1,37 @@
+<?php
+    // Verifica se o formulário de login foi enviado
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Obtém os valores digitados pelo usuário
+        $user = $_POST['username'];
+        $senha = $_POST['password'];
+
+        // Realiza a conexão com o banco de dados
+        $conexao = mysqli_connect("localhost", "root", "", "ProjetoRedeSocial");
+
+        // Verifica se a conexão foi bem-sucedida
+        if ($conexao === false) {
+            die("Erro de conexão com o banco de dados: " . mysqli_connect_error());
+        }
+
+        // Consulta SQL para verificar se os dados de login estão corretos
+        $sql = "SELECT * FROM Usuario WHERE username = '$user' AND senha = '$senha'";
+        $resultado = mysqli_query($conexao, $sql);
+
+        // Verifica se a consulta retornou algum resultado
+        if (mysqli_num_rows($resultado) === 1) {
+            // Obtém o nome do usuário
+            $row = mysqli_fetch_assoc($resultado);
+            $nomeUsuario = $row['username'];
+
+            // Redireciona para a página inicial
+            header("Location: paginainicial.html?nome=$nomeUsuario");
+            exit();
+        } else {
+            // Login inválido, exibe uma mensagem de erro
+            echo "Usuário ou senha inválidos";
+        }
+
+        // Fecha a conexão com o banco de dados
+        mysqli_close($conexao);
+    }
+?>
