32267a0b9a99a44fe984ddf93c338e5db28c1ebd
Rafael Cabral <melloraf.cabral@gmail.com>
2023-12-27 22:44:39 -0300

    ajustando css

--- resultado_busca.php
+++ resultado_busca.php
@@ -8,21 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="estilo-paginainicial.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
     <title>Página Inicial</title>
-
-    <style>
-
-      body{
-        background: rgba(162, 170, 172, .4);
-      } 
-
-      header{
-        background: url(img/fundo.jpg);
-      }
-
-    </style>
 
   </head>
 
@@ -30,7 +19,9 @@
 
     <header class="navbar navbar-expand-lg navbar-dark header with-bg-image">
 
-      <a class="navbar-brand" href="paginainicial.html"><img src="img/rede-social.png" style="height: 60px; width: 60px;"></a>
+      <a class="navbar-brand" href="paginainicial.html">
+        <img src="img/rede-social.png" id="logo" >
+      </a>
 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -39,26 +30,32 @@
       <div class="collapse navbar-collapse" id="navbarNav">
 
         <!-- formulário -->
-        <form class="form-inline ml-auto" action="processar_busca.php" method="post">
-          <input type="text" id="username" name="username" class="form-control mr-1" placeholder="Pesquisar Usuário" style="width:400px;">
+        <form class="form-inline ml-3" action="processar_busca.php" method="post">
+          <input type="text" id="username" name="username" class="form-control mr-1" placeholder="Pesquisar Usuário">
           <button>
-            <img src="img/lupa.png" style="width: 25px; height: 25px; cursor: pointer;">
+            <img src="img/lupa.png">
           </button>
         </form>
         <!-- fim do formulário -->
 
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item active p-2">
-              <a class="nav-link" href="#">Home &nbsp; &nbsp; &nbsp; |</a>
+            <li class="nav-item active pr-3">
+              <a class="nav-link" href="paginainicial.html">Home</a>
             </li>
-            <li class="nav-item p-2">
-              <a class="nav-link" href="">Chat &nbsp; &nbsp; &nbsp; |</a>
+            <div class="dropdown-divider"></div>
+
+            <li class="nav-item pr-3">
+              <a class="nav-link" href="">Chat</a>
             </li>
-            <li class="nav-item p-2">
-              <a class="nav-link" href="">Notificações &nbsp; &nbsp; &nbsp; |</a> 
+            <div class="dropdown-divider"></div>
+            
+            <li class="nav-item pr-3">
+              <a class="nav-link" href="">Notificações</a> 
             </li>
-            <li class="nav-item p-2">
-              <a class="nav-link" href="login.html"><img src="img/power.png" style="width: 40px; height: 30px;"></a>
+            <div class="dropdown-divider"></div>
+            
+            <li class="nav-item pr-3">
+              <a class="nav-link" href="login.html"><img src="img/logout.png" id="logout"></a>
             </li>
           </ul>
 
